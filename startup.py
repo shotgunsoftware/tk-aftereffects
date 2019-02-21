1612387943e6233fd5899bb4e7a7849f5251d1a2
Martin Minsel <martin@rndeep.com>
2019-02-21 11:33:55 +0100

    deprecating bootstrap py and adding a new logic to find the tk-framework-adobe on startup

--- startup.py
+++ startup.py
@@ -12,8 +12,13 @@ import imp
 import os
 import sys
 
+import sgtk
 import sgtk.platform.framework
 from sgtk.platform import SoftwareLauncher, SoftwareVersion, LaunchInformation
+
+
+class EngineConfigurationError(sgtk.TankError):
+    pass
 
 
 class AfterEffectsLauncher(SoftwareLauncher):
@@ -64,17 +69,8 @@ class AfterEffectsLauncher(SoftwareLauncher):
         :returns: :class:`LaunchInformation` instance
         """
 
-        # find the bootstrap script and import it.
-        # note: all the business logic for how to launch is
-        #       located in the python/startup folder to be compatible
-        #       with older versions of the launch workflow
-        bootstrap_python_path = os.path.join(self.disk_location, "python", "startup", "bootstrap.py")
-        mod_file = open(bootstrap_python_path, "r")
-        bootstrap = imp.load_module('bootstrap', mod_file, 'tk-aftereffectscc.bootstrap', ('.py', 'U', 1))
-        mod_file.close()
-
         # determine all environment variables
-        required_env = bootstrap.compute_environment()
+        required_env = self.compute_environment()
 
         # Add std context and site info to the env
         std_env = self.get_standard_plugin_environment()
@@ -105,9 +101,11 @@ class AfterEffectsLauncher(SoftwareLauncher):
         all_sw_versions = []
 
         for executable_path, tokens in self._glob_and_match(
-            self.EXECUTABLE_MATCH_TEMPLATES[sys.platform], self.COMPONENT_REGEX_LOOKUP
-        ):
-            self.logger.debug("Processing %s with tokens %s", executable_path, tokens)
+                    self.EXECUTABLE_MATCH_TEMPLATES[sys.platform],
+                    self.COMPONENT_REGEX_LOOKUP):
+            self.logger.debug("Processing %s with tokens %s",
+                        executable_path,
+                        tokens)
             # extract the components (default to None if not included). but
             # version is in all templates, so should be there.
             executable_version = tokens.get("version")
@@ -125,3 +123,110 @@ class AfterEffectsLauncher(SoftwareLauncher):
                 self.logger.debug(reason)
 
         return all_sw_versions
+
+    def compute_environment(self):
+        """
+        Return the env vars needed to launch the After Effects plugin.
+
+        This will generate a dictionary of environment variables
+        needed in order to launch the After Effects plugin.
+
+        :returns: dictionary of env var string key/value pairs.
+        """
+        env = {}
+
+        framework_location = self.__get_adobe_framework_location()
+        if framework_location is None:
+            raise EngineConfigurationError(("The tk-framework-adobe "
+                    "could not be found in the current environment. "
+                    "Please check the log for more information."))
+
+        self.__ensure_framework_is_installed(framework_location)
+
+        # set the interpreter with which to launch the CC integration
+        env["SHOTGUN_ADOBE_PYTHON"] = sys.executable
+        env["SHOTGUN_ADOBE_FRAMEWORK_LOCATION"] = framework_location
+        env["SHOTGUN_ENGINE"] = "tk-aftereffectscc"
+
+        # We're going to append all of this Python process's sys.path to the
+        # PYTHONPATH environment variable. This will ensure that we have access
+        # to all libraries available in this process in subprocesses like the
+        # Python process that is spawned by the Shotgun CEP extension on launch
+        # of an Adobe host application. We're appending instead of setting because
+        # we don't want to stomp on any PYTHONPATH that might already exist that
+        # we want to persist when the Python subprocess is spawned.
+        sgtk.util.append_path_to_env_var(
+            "PYTHONPATH",
+            os.pathsep.join(sys.path),
+        )
+        env["PYTHONPATH"] = os.environ["PYTHONPATH"]
+
+        return env
+
+    def __get_adobe_framework_location(self):
+        """
+        This helper method will query the current disc-location for the configured
+        tk-adobe-framework.
+
+        This is necessary, as the the framework relies on an environment variable
+        to be set by the parent engine and also the CEP panel to be installed.
+
+        TODO: When the following logic was implemented, there was no way of
+            accessing the engine's frameworks at launch time. Once this is
+            possible, this logic should be replaced.
+
+        Returns (str or None): The tk-adobe-framework disc-location directory path
+            configured under the tk-multi-launchapp
+        """
+
+        engine =sgtk.platform.current_engine()
+        env_name = engine.environment.get("name")
+        if env_name is None:
+            self.logger.warn(("The current environment of engine {!r} "
+                "seems to be invalid. No name found. Environment: "
+                "{!r}").format(engine.name, engine.environment))
+            return
+
+        env = engine.tank.pipeline_configuration.get_environment(env_name)
+        engine_desc = env.get_engine_descriptor("tk-aftereffectscc")
+        if env_name is None:
+            self.logger.warn(("The current environment {!r} "
+                "is not configured to run the tk-aftereffectscc "
+                "engine. Please add the engine to your env-file: "
+                "{!r}").format(env, env.disk_location))
+            return
+
+        framework_name = None
+        for req_framework in engine_desc.get_required_frameworks():
+            if req_framework.get("name") == "tk-framework-adobe":
+                name_parts = [req_framework["name"]]
+                if req_framework.has_key("version"):
+                    name_parts.append(req_framework["version"])
+                framework_name = "_".join(name_parts)
+                break
+        else:
+            self.logger.warn(("The engine tk-aftereffectscc must have "
+                "the tk-framework-adobe configured in order to run"))
+            return
+
+        desc = env.get_framework_descriptor(framework_name)
+        return desc.get_path()
+
+
+    def __ensure_framework_is_installed(self, framework_location):
+        """
+        This method calls the frameworks CEP extension installation
+        logic.
+        """
+
+        # TODO: The following import should be replaced with
+        # a more a call like import_framework, once one has
+        # access to the configured frameworks at engine start.
+        bootstrap_python_path = os.path.join(framework_location, "python")
+
+        sys.path.insert(0, bootstrap_python_path)
+        import startup_utils
+        sys.path.remove(bootstrap_python_path)
+
+        # installing the CEP extension.
+        startup_utils.ensure_extension_up_to_date(self.logger)

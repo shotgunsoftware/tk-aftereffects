8b329deaf004dffb52fb35fa39174d00ace691a9
Philip Scadding <philip.scadding@autodesk.com>
2019-10-18 11:55:04 +0100

    Adds support for opening a file on startup

--- engine.py
+++ engine.py
@@ -228,6 +228,16 @@ class AfterEffectsEngine(sgtk.platform.Engine):
             dict(),
         )
 
+        # Normally the bootstrap logic would handle the file open, but since the bootstrap logic is handled by
+        # the adobe framework, and is generic, we should handle it here.
+        file_to_open = os.environ.get("SGTK_FILE_TO_OPEN")
+
+        if file_to_open:
+            # open the specified script
+            self.adobe.app.open(self.adobe.File(file_to_open))
+            # clear the environment variable after loading so that it doesn't get reopened on an engine restart.
+            del os.environ["SGTK_FILE_TO_OPEN"]
+
     def destroy_engine(self):
         """
         Called when the engine should tear down itself and all its apps.
